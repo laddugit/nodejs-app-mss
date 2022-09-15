39b08dc154eb3a76f0fe9dd24f54bce232c8a012
laddugit <64884390+laddugit@users.noreply.github.com>
2022-09-15 15:00:48 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.233.224.241:9000/',
+  serverUrl: 'http://3.109.185.56:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
